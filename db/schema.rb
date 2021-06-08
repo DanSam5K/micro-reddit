61d8ff2a7dca6a8402e46512e146d1d13ef0d3b6
DanteAlonsoHT <dante1818@hotmail.com>
2021-06-08 16:37:05 -0500

    Add Post model and associate with Author model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_205739) do
+ActiveRecord::Schema.define(version: 2021_06_08_213120) do
 
   create_table "authors", force: :cascade do |t|
     t.string "username"
@@ -20,4 +20,14 @@ ActiveRecord::Schema.define(version: 2021_06_08_205739) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  add_foreign_key "posts", "authors"
 end

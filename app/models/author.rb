095c4e36b00069d1dff9a15b12272a9748fcff67
Daniel Samuel <dansam5000@gmail.com>
2021-06-08 23:53:11 +0100

    Add active record validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+  has_many :posts
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum:6, maximum:16}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :author
+
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true, length: { minimum: 10 }
 end

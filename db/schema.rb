619161b2e1d46661075d2898b1dc04e7fb021723
DanteAlonsoHT <dante1818@hotmail.com>
2021-06-08 23:23:44 -0500

    Add Comment model with reference to Post model and Author model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_213120) do
+ActiveRecord::Schema.define(version: 2021_06_09_041758) do
 
   create_table "authors", force: :cascade do |t|
     t.string "username"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2021_06_08_213120) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "topic"
+    t.text "body"
+    t.integer "author_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,5 +40,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_213120) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  add_foreign_key "comments", "authors"
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "authors"
 end

6a46f780779e23787fc8ccde13a108df0a6b6d61
Daniel Samuel <dansam5000@gmail.com>
2021-06-09 17:23:31 +0100

    fix rubocop an linters errors

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,5 +4,5 @@ class Author < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: {minimum:6, maximum:16}
+  validates :password, presence: true, length: { minimum: 6, maximum: 16 }
 end

2fff5f888b90648b5389f2d6e82c5b2ef6924779
DanteAlonsoHT <dante1818@hotmail.com>
2021-06-09 16:10:54 -0500

    Add has_may comments  for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author
   belongs_to :comment
 
+  has_many :comments
+
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true, length: { minimum: 10 }
 end
